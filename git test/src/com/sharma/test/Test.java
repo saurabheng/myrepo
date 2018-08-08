79c172cc3614a87b9f2e0df233462feecd8e9c29
Saurabh Sharma -X (saursha5 - ZENSAR TECHNOLOGIES INC at Cisco) <saursha5@partnet.cisco.com>
2018-08-08 12:38:43 +0530

    Adding a new commit for sudhir

---
+++ git test/src/com/sharma/test/Test.java
@@ -0,0 +1,15 @@
+package com.sharma.test;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int add(int a, int b) {
+		return a+b;
+	}
+	public static void main(String[] args) {
+		Test t =new Test();
+	}
+}
